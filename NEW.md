03cc32d75cef2ccc4fe93e2a33ea25e0baa8641f
niderman <viktor.niderman@gmail.com>
2025-05-22 23:38:33 +0100

    [main-B1] cards for B1 level

---
+++ NEW.md
@@ -0,0 +1,18 @@
+# Front:
+to ponder [sound:to_ponder.mp3]
+
+# Back:
+<b>To think carefully and deeply about something, especially for a noticeable amount of time.</b>
+<hr>
+<i>She pondered the question before answering.</i>
+<hr>
+ponder a question / ponder the meaning / ponder over something
+<hr>
+🔹 Synonyms:  
+think about, consider
+<hr>
+[img:ponder.jpg]
+<hr>
+<details><summary>Translate</summary>
+обдумывать/размышлять/раздумывать
+</details>
--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from pathlib import Path
 import re
 import uuid
 
-from utils.anki import invoke, store_media_file, add_note
+from utils.anki import store_media_file, add_note
 from utils.audio import generate_audio
 
 # Load environment variables from .env file
 load_dotenv(sys.argv[1])
 
-def add_new_words_to_deck(deck_name, new_words_path, limit, language, tld):
+def add_new_words_to_deck(deck_name, new_words_path, language, tld):
     print("Adding new words from ", new_words_path)
     try:
         file_path = Path(new_words_path)
@@ -22,7 +22,7 @@ def add_new_words_to_deck(deck_name, new_words_path, limit, language, tld):
 
         for entry in entries:
             lines = entry.strip().split("\n")
-            if len(lines) < 2:
+            if len(lines) < 7:
                 print("Not enough lines in entry. Skipping...")
                 continue
 
@@ -35,44 +35,62 @@ def add_new_words_to_deck(deck_name, new_words_path, limit, language, tld):
                 front += f'\n[sound:{audio_filename_front}]'
 
 
-            back_translation = lines[1].strip()   # Translation
+            image_line = lines[1].strip()
+            img_match = re.match(r'!\[.*?\]\((.*?)\)', image_line)
+            image = img_match.group(1)
 
-            # Processing images and expressions
-            images = []
-            additional_lines = []
+            word_image = ""
+            word_description = lines[2].strip()
+            word_examples = lines[3].strip()
+            word_phrases = lines[4].strip()
+            word_synonyms = lines[5].strip()
+            word_translation = lines[6].strip()
 
-            for line in lines[2:]:
-                img_match = re.match(r'!\[.*?\]\((.*?)\)', line)
-                if img_match:
-                    img_filename = img_match.group(1)
-                    images.append(img_filename)
+            try:
+                img_path = Path(os.path.dirname(new_words_path)) / image
+                if img_path.exists():
+                    with open(img_path, "rb") as f:
+                        img_data = f.read()
+
+                    unique_filename = f"{uuid.uuid4()}_{image}"
+                    word_image = f'<img src="{unique_filename}">'
+                    store_media_file(unique_filename, img_data)
                 else:
-                    additional_lines.append(line)
+                    print(f"Image file '{image}' not found. Skipping upload.")
+            except Exception as e:
+                print(f"Error uploading image '{image}': {e}")
 
-            # Form the Back field
-            back_parts = [back_translation]
+            back_content = f"""
+<div style="font-family: sans-serif; max-width: 600px; margin: 0 auto; border-radius: 12px; background: #1b1b1b; color: white; padding: 1em;">
 
-            if images:
-                for img in images:
-                    try:
-                        img_path = Path(os.path.dirname(new_words_path)) / img
-                        if img_path.exists():
-                            with open(img_path, "rb") as f:
-                                img_data = f.read()
+  <div style="margin-bottom: 1em; font-weight: bold;">
+    {word_description}
+  </div>
 
-                            unique_filename = f"{uuid.uuid4()}_{img}"
-                            back_parts.append(f'<br><br><img src="{unique_filename}">')
-                            store_media_file(unique_filename, img_data)
-                        else:
-                            print(f"Image file '{img}' not found. Skipping upload.")
-                    except Exception as e:
-                        print(f"Error uploading image '{img}': {e}")
+  <div style="font-size: 0.85em; border-radius: 8px; padding: 0.65em 1em; background-color: #263543; margin-bottom: 0.75em; position: relative; color: #349ca7;">
+    <div style="position: absolute; top: 0.2em; right: 0.2em; font-size: 0.6em; font-weight: bold; color: #0077cc;">💬</div>
+    <i>{word_examples}</i>
+  </div>
 
-            if additional_lines:
-                for line in additional_lines:
-                    back_parts.append(f'<br><br>{line}')
+  <div style="font-size: 0.85em; border-radius: 8px; padding: 0.65em 1em; background-color: #222e1e; margin-bottom: 0.75em; position: relative; color: #6bb86b;">
+    <div style="position: absolute; top: 0.2em; right: 0.2em; font-size: 0.6em; font-weight: bold; color: #2d8a34;">🧩</div>
+    <i>{word_phrases}</i>
+  </div>
 
-            back_content = ''.join(back_parts)
+  <div style="font-size: 0.85em; border-radius: 8px; padding: 0.65em 1em; background-color: #30253d;; margin-bottom: 0.75em; position: relative; color: #c76cc5;">
+    <div style="position: absolute; top: 0.2em; right: 0.2em; font-size: 0.6em; font-weight: bold; color: #8e44ad;">🔁</div>
+    <i>{word_synonyms}</i>
+  </div>
+
+  <div style="margin: 1em 0;">{word_image}</div>
+
+  <details style="margin-top: 1em;">
+    <summary style="cursor: pointer; color: #4a6373; font-weight: 500;">Translate</summary>
+    <div style="margin-top: 0.5em;">{word_translation}</div>
+  </details>
+
+</div>
+            """
 
             # Add the note
             add_note_result = add_note(deck_name, front, back_content)
@@ -85,29 +103,14 @@ def add_new_words_to_deck(deck_name, new_words_path, limit, language, tld):
 def main():
     deck_name = os.getenv('DECK_NAME')
     new_words_path = os.getenv('NEW_WORDS_FILE_NAME')
-    is_add_new = os.getenv('ADD_NEW')
-    is_rewrite_old = os.getenv('REWRITE_OLD')
-    limit = os.getenv('LIMIT')
     lang = os.getenv('LANG')
     tld = os.getenv('TLD')
 
-    if not deck_name:
+    if not new_words_path or not deck_name:
         print("Deck name not specified in the .env file. Exiting.")
         return
 
-    if is_rewrite_old == 'True':
-        print("Rewrite old is set - DISABLE")
-    if is_add_new == 'True':
-        if not new_words_path:
-            print("Deck name not specified in the .env file. Exiting.")
-            return
-        add_new_words_to_deck(deck_name, new_words_path, limit, lang, tld)
-
-
-    # Add new words
-
-
-    # process_existing_cards(deck_name)
+    add_new_words_to_deck(deck_name, new_words_path, lang, tld)
 
 if __name__ == "__main__":
     main()
